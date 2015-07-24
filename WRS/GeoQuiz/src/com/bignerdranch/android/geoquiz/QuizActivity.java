3f3b103ffe226577c6d8981678b73f60eb1ac585
wrsophia <1551717714@qq.com>
2015-07-24 14:19:35 +0800

    2015_7_24

---
+++ WRS/GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -0,0 +1,73 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuizActivity extends Activity {
+
+	private Button mTrueButton;
+	private Button mFalseButton;
+	private Button mNextButton;
+	private TextView mQuestionTextView;
+	
+	private TrueFalse[] mQuestionBank = new TrueFalse[]{
+			new TrueFalse(R.string.question_oceans, true),
+			new TrueFalse(R.string.question_mideast, false),
+			new TrueFalse(R.string.question_africa, false),
+			new TrueFalse(R.string.question_americas, true),
+			new TrueFalse(R.string.question_asia, true),
+	};
+	
+	private int mCurrentIndex = 0;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+        
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+        
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
+        	@Override
+        	public void onClick(View v){
+        		Toast.makeText(QuizActivity.this, R.string.incorrect_toast,
+        				Toast.LENGTH_SHORT).show();
+        	}
+        });
+        mFalseButton = (Button)findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener(){
+        	@Override
+        	public void onClick(View v){
+        		Toast.makeText(QuizActivity.this, R.string.correct_toast,
+        				Toast.LENGTH_SHORT).show();
+        		
+        	}
+        });
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener(){
+        	@Override
+        	public void onClick(View v){
+        		mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+        		int question = mQuestionBank[mCurrentIndex].getQuestion();
+        		mQuestionTextView.setText(question);
+        	}
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.quiz, menu);
+        return true;
+    }
+    
+}
