c29bac55e43194989a46fd0ef151b3a2a66cd1be
wrsophia <1551717714@qq.com>
2015-07-27 10:08:22 +0800

    3rd modification
    改了上次的bug，旋转屏幕后也会回到当前状态

--- WRS/GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ WRS/GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
+	
+	private static final String TAG = "QuizActivity";
+	private static final String KEY_INDEX = "index";
 
 	private Button mTrueButton;
 	private Button mFalseButton;
@@ -45,9 +49,18 @@ public class QuizActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
         
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        mQuestionTextView.setOnClickListener(new View.OnClickListener(){
+        	@Override
+        	public void onClick(View v){
+        		mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+  
+        		updateQuestion();
+        	}
+        });
         
         mTrueButton = (Button)findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener(){
@@ -86,10 +99,44 @@ public class QuizActivity extends Activity {
         	}
         });
         
+        if(savedInstanceState != null){
+        	mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+        
         updateQuestion();
     }
+    
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+    	super.onSaveInstanceState(savedInstanceState);
+    	Log.i(TAG, "onSaveInstanceState");
+    	savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
 
+    @Override
+    public void onStart(){
+    	super.onStart();
+    	Log.d(TAG,"onStart() called");
+    }
 
+    @Override
+    public void onPause(){
+    	super.onPause();
+    	Log.d(TAG, "onPause() called");
+    }
+    
+    @Override
+    public void onStop(){
+    	super.onStop();
+    	Log.d(TAG,"onStop() called");
+    }
+    
+    @Override
+    public void onDestroy(){
+    	super.onDestroy();
+    	Log.d(TAG,"onDestroy() called");
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
